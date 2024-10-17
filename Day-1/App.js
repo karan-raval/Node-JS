2610d56c45ed6e567d308e919ffd50f67131344b
karan-raval <karanraval424@gmail.com>
2024-10-17 08:16:11 +0530

    Local Modules

---
+++ Day-1/App.js
@@ -0,0 +1,9 @@
+//  modules
+   //--> Local module
+
+ const {add,sub,mult} = require("./Math.js")
+
+
+   console.log(add(4,5))
+   console.log(sub(4,5))
+   console.log(mult(4,5))
---
+++ Day-1/Math.js
@@ -0,0 +1,17 @@
+const add = (a,b)=>{
+    return a + b;
+}
+const sub = (a,b)=>{
+    return a - b
+}
+const mult = (a,b)=>{
+    return a * b;
+}
+
+
+
+ module.exports = {
+    add,
+    sub,
+    mult
+ }
