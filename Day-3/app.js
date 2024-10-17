7de43f9251bcd9653a2d62af64d0a4c696208f63
karan-raval <karanraval424@gmail.com>
2024-10-17 18:36:47 +0530

    HTTP

--- Day-3/app.js
+++ Day-3/app.js
@@ -1,17 +1,21 @@
 const http = require("http");
-
+const path = require("path");
+const fs = require("fs");
+// let home = path.join(__dirname,"index.html")
+// console.log(home)
 let server = http.createServer((req, res) => {
-  if (res.url == "/") {
-    res.end("hellow World");
-  } else if (res.url == "/about") {
+  if (req.url == "/") {
+    let a = fs.readFileSync("index.html",'utf-8')
+    res.end(a);
+  } else if (req.url == "/about") {
     res.end("about Page");
-  } else if (res.url == "/contact") {
+  } else if (req.url == "/contact") {
     res.end("contact form Page");
   } else {
     res.end("Page Not Found !!!!!");
   }
-});
+})
 
 server.listen(3000, () => {
   console.log("server running at 3000");
-});
+})
