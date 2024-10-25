cd29b2d8b5fff0cf991d2120f18a40514157bea7
karan-raval <karanraval424@gmail.com>
2024-10-25 12:53:03 +0530

    ejs CRUD Opration

---
+++ Day-07/app.js
@@ -0,0 +1,54 @@
+const express = require("express")
+
+
+const app = express()
+
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended : true}))
+
+let arr = [];
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+
+
+app.post("/",(req,res)=>{
+    arr.push({...req.body,id : arr.length+1 + Date.now()})
+    console.log(arr)
+    res.redirect("/data")
+})
+
+
+app.get("/data",(req,res)=>{
+    res.render("data.ejs",{arr :arr})
+})
+
+app.get("/data/:id",(req,res)=>{
+    let a = req.params.id
+    arr = arr.filter((el)=>el.id != a)
+    res.redirect("/data")
+})
+
+app.get("/edit/:id",(req,res)=>{
+    let a = arr.filter((el)=>el.id == req.params.id)
+    console.log(a)
+    res.render("edit.ejs",{data : a})
+})
+
+app.post("/edit/:id",(req,res)=>{
+    let a = req.params.id;
+    arr = arr.map((el)=>{
+        if(el.id == a){
+            return {...el,...req.body}
+        }else{
+            return el;
+        }
+    })
+    res.redirect("/data")
+})
+
+
+app.listen(1234,()=>{
+    console.log("Server running on Port 1234")
+})
