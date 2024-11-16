042b69273fd2b825b42ee14cbf436277a28fc323
karan-raval <karanraval424@gmail.com>
2024-11-16 13:25:19 +0530

    Front-End App.jsx

--- Day-10/Front-End/src/App.jsx
+++ Day-10/Front-End/src/App.jsx
@@ -27,7 +27,14 @@ function App() {
  }
   return (
     <>
-      
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text"  placeholder='Book Name' name='bookName'  onChange={handleChange}/>
+          <input type="text" placeholder='Description' name='des'  onChange={handleChange}/>
+          <input type="text" placeholder='Author' name='author'  onChange={handleChange}/>
+          <input type="text" placeholder=' Image URL' name='image'  onChange={handleChange}/>
+          <input type="text"  placeholder='Price' name='price'   onChange={handleChange}/>
+          <input type="submit" />
+      </form>
     </>
   )
 }
