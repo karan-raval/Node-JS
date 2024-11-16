9ed4702427a4fd0c7540b1589681effe662f04c7
karan-raval <karanraval424@gmail.com>
2024-11-16 13:24:54 +0530

    Front-End App.jsx

--- Day-10/Front-End/src/App.jsx
+++ Day-10/Front-End/src/App.jsx
@@ -2,7 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [state,setState] = useState({
+    bookName : "",
+    des : "",
+    author : "",
+    image : "",
+    price : ""
+ })
+ const handleChange = (e)=>{
+    let {name,value} = e.target
+    setState({...state, [name] : value})
+ }
+ const handleSubmit = (e)=>{
+   e.preventDefault()
+    fetch(`http://localhost:8080/addBook`,{
+     method : "POST",
+     headers : {
+        "Content-Type" : "application/json"
+     },
+     body : JSON.stringify(state)
+    })
+    .then((Res)=>Res.json())
+    .then((Res)=>console.log(Res))
+ }
   return (
     <>
       
