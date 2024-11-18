e1bb2c6b163a45d25bc69d1721ef0974a3f65479
karan-raval <karanraval424@gmail.com>
2024-11-18 23:38:42 +0530

    CRUD Opration With React

--- Day-10/Back-End/app.js
+++ Day-10/Back-End/app.js
@@ -1,25 +1,23 @@
-const express = require("express")
-const connect = require("./Config/db")
-const cors = require("cors")
-const BookModel = require("./Model/BookModel")
-const app = express()
+const express = require("express");
+const connect = require("./Config/db");
+const cors = require("cors");
+const BookModel = require("./Model/BookModel");
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.get("/", async (req, res) => {
+  let books = await BookModel.find();
+  res.send(books);
+});
 
-app.get("/",async(req,res)=>{
-    let books = await BookModel.find()
-   res.send(books)
-})
+app.post("/addBook", async (req, res) => {
+  let a = await BookModel.create(req.body);
+  //  res.send({data : a})
+  // res.redirect('/allbook')
+});
 
-app.post("/addBook",async(req,res)=>{
-     let a = await BookModel.create(req.body)
-    //  res.send({data : a})
-    // res.redirect('/allbook')
-})
-
-
-app.listen(1234,()=>{
-    connect()
-    console.log("Server is running at port 1234")
-})
+app.listen(1234, () => {
+  connect();
+  console.log("Server is running at port 1234");
+});
