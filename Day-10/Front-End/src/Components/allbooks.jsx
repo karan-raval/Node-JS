6eb75c44c30c434e05d2a8d2b48c1ff904da53dd
karan-raval <karanraval424@gmail.com>
2024-11-19 13:57:23 +0530

    CRUD Opration With React

--- Day-10/Front-End/src/Components/allbooks.jsx
+++ Day-10/Front-End/src/Components/allbooks.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Allbooks = () => {
+  fetch("http://localhost:1234/addBook")
+  .then(Res => Res.json())
+  .then(Res=>Res)
   return (
     <>
-        
+
     </>
   )
 }
