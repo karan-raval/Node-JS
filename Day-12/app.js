ab66a59991a40e87bb4afd8ba791c1026d58a246
karan-raval <karanraval424@gmail.com>
2024-11-21 15:13:04 +0530

    CRUD Opration With ejs

--- Day-12/app.js
+++ Day-12/app.js
@@ -5,6 +5,41 @@ app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
+app.get("/",async(req,res)=>{
+  let Books = await BookModel.find()
+  res.render("index.ejs",{Books : Books})
+})
+
+
+app.get("/addBook",(req,res)=>{
+  res.render("BookForm.ejs")
+})
+
+
+app.post("/addBook",async(req,res)=>{
+   await BookModel.create(req.body)
+   res.redirect("/")
+})
+
+
+app.get("/deleteBook/:id",async (req,res)=>{
+  let {id} = req.params
+  await BookModel.findByIdAndDelete(id)
+  res.redirect("/")
+})
+
+app.get("/editBook/:id",async(req,res)=>{
+  let {id} = req.params;
+  let data = await BookModel.findById(id)
+  
+  res.render("editForm.ejs",{data : data})
+})
+
+app.post("/editBook/:id",async(req,res)=>{
+  const {id} = req.params
+  await BookModel.findByIdAndUpdate(id,req.body)
+  res.redirect("/")
+})
 
 
 app.listen(3131, () => {
