c9221110ebbeaacf46858bf213744cabc34a9e6b
karan-raval <karanraval424@gmail.com>
2024-11-22 12:51:18 +0530

    Controller And Router

--- Day-13/app.js
+++ Day-13/app.js
@@ -1,10 +1,8 @@
-const express=require('express')
-const app=express()
-const connect =require('./config/server')
+const express = require("express");
+const app = express();
+const connect = require("./config/server");
 
-
-
-app.listen(3333,()=>{
-    connect()
-    console.log("server started at 3333")
-})
+app.listen(3333, () => {
+  connect();
+  console.log("server started at 3333");
+});
