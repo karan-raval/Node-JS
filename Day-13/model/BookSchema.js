6caf376ac3a66fa006d8d8990b81bdb5356ec472
karan-raval <karanraval424@gmail.com>
2024-11-23 18:35:31 +0530

    Schema in Mongoose

--- Day-13/model/BookSchema.js
+++ Day-13/model/BookSchema.js
@@ -1,14 +1,13 @@
+const mongoose = require("mongoose");
 
-const mongoose =require('mongoose')
+const BookSchema = mongoose.Schema({
+  bookname: { type: String },
+  authorname: { type: String },
+  price: { type: String },
+  image: { type: String },
+  description: { type: String },
+});
 
-const BookSchema =mongoose.Schema({
-bookname : {type:string},
-authorname: {type:string},
-price: {type:string},
-image: {type:string},
-description: {type:string},
-})
+const BookModel = mongoose.model("allBooks", BookSchema);
 
-const BookModel=mongoose.model("allBooks",BookSchema)
-
-module.exports=BookModel
+module.exports = BookModel;
