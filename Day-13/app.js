5f391d5fbc786fee7496960768d41f18af5dde99
karan-raval <karanraval424@gmail.com>
2024-11-24 11:40:18 +0530

    app.js

--- Day-13/app.js
+++ Day-13/app.js
@@ -7,6 +7,10 @@ app.set("view engine",'ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
+app.use("/book",BookRouter)
+// app.use("/user",UserRoute)
+
+
 app.listen(3333, () => {
   connect();
   console.log("server started at 3333");
