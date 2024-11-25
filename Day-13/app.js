bca23da9f93b0318a3617539d864a32dc20bf83d
karan-raval <karanraval424@gmail.com>
2024-11-25 11:57:12 +0530

    app.js

--- Day-13/app.js
+++ Day-13/app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const connect = require("./config/server");
-const BookRouter=require('./Routes/bookRoutes')
+const BookRouter = require("./Routes/bookRoutes");
 const app = express();
 
-app.set("view engine",'ejs')
-app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use("/book",BookRouter)
+app.use("/book", BookRouter);
 // app.use("/user",UserRoute)
-
 
 app.listen(3333, () => {
   connect();
