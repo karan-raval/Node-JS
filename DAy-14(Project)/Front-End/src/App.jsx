3b3f508154d7234eab5ef127a372579695cf6aa9
karan-raval <karanraval424@gmail.com>
2024-11-26 19:29:59 +0530

    Movie Web App

--- DAy-14(Project)/Front-End/src/App.jsx
+++ DAy-14(Project)/Front-End/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div class="wrapper">
+    <div class="search-block">
+        <a href="/">
+            <h1>TheMovieDB</h1>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <form id="search-form">
+            <input type="text" id="query" placeholder="Search movies..." />
+            <button class="btn" type="submit">Search</button>
+        </form>
+        <div class="col">
+          <div class="card">
+            <a class="card-media" href="./img-01.jpeg">
+              <img src="${movie.poster_path}" alt="PUBG Mobile" width="100%" />
+            </a>
 
-export default App
+            <div class="card-content">
+              <div class="card-cont-header">
+                <div class="cont-left">
+                  <h3 style={{fontWeight: 600}}>$movie.original_title</h3>
+                  <span style={{color: '#12efec'}}>$movie.release_date</span>
+                </div>
+                <div class="cont-right">
+                  <a href="${movie.poster_path}" target="_blank" class="btn">See image</a>
+                </div>
+              </div>
+
+              <div class="describe">
+                $movie.overview
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="row" id="root"></div>
+</div>
+  );
+};
+
+export default App;
