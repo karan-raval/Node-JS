b1b7d051b38f5a589cd409abeb4e5e7af0669e9c
karan-raval <karanraval424@gmail.com>
2024-11-26 21:40:26 +0530

    server in Movie Web App

---
+++ DAy-14(Project)/Back-End/Model/MovieSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const MovieSchema=mongoose.Schema({
+    type:{type:String},
+    moviename:{type:String},
+    imdbrating:{type:String},
+    Gener:{type:String},
+    Year:{type:String},
+})
+const MovieModel=mongoose.model('movie',MovieSchema)
+
+module.exports=MovieModel
