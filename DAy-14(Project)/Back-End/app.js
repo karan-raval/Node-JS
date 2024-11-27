6c1adc38093fe561209b12922c433af5ca9a960a
karan-raval <karanraval424@gmail.com>
2024-11-27 11:22:28 +0530

    App.js in Movie Web App

--- DAy-14(Project)/Back-End/app.js
+++ DAy-14(Project)/Back-End/app.js
@@ -1,7 +1,10 @@
 const express =require('express')
 const connet = require('./config/server')
-
+const cors =require('cors')
 const app=express()
+
+app.use(express.json())
+app.use(cors())
 
 app.listen(3344,()=>{
     connet()
