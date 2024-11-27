544a7da1f6c88870429bc1a38646148cf5e02042
karan-raval <karanraval424@gmail.com>
2024-11-27 14:49:28 +0530

    app in Movie Web App

--- DAy-14(Project)/Back-End/app.js
+++ DAy-14(Project)/Back-End/app.js
@@ -1,15 +1,15 @@
-const express =require('express')
-const connet = require('./config/server')
-const cors =require('cors')
-const MovieRouter =require('./Routes/MovieRoutes')
-const MovieModel = require('./Model/MovieSchema')
-const app=express()
+const express = require("express");
+const connet = require("./config/server");
+const cors = require("cors");
+const MovieRouter = require("./Routes/MovieRoutes");
+const MovieModel = require("./Model/MovieSchema");
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use('/',MovieRouter)
+app.use(express.json());
+app.use(cors());
+app.use("/", MovieRouter);
 
-app.listen(3344,()=>{
-    connet()
-    console.log('server running at 3344')
-})
+app.listen(3344, () => {
+  connet();
+  console.log("server running at 3344");
+});
