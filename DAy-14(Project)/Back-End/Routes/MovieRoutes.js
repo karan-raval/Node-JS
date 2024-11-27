1c2144f35053845882714c0336aa55501ae7b3ae
karan-raval <karanraval424@gmail.com>
2024-11-27 14:48:02 +0530

    Router in Movie Web App

--- DAy-14(Project)/Back-End/Routes/MovieRoutes.js
+++ DAy-14(Project)/Back-End/Routes/MovieRoutes.js
@@ -1,37 +1,36 @@
-const express=require('express')
-const MovieModel = require('../Model/MovieSchema')
-const MovieRouter = express.Router()
-const multer = require("multer")
-const path = require("path")
-const fs = require("fs")
+const express = require("express");
+const MovieModel = require("../Model/MovieSchema");
+const MovieRouter = express.Router();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const s = multer.diskStorage({
-    destination : (req,file,cb)=>{
-        cb(null,path.join(__dirname,"../public/assets"))
-    },
-    filename : (req,file,cb)=>{
-       cb(null,file.originalname)
-    }
-}) 
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../public/assets"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-const Data = multer({storage : s})
+const Data = multer({ storage: s });
 
+MovieRouter.post("/addmovie", Data.single("poster"), async (req, res) => {
+  try {
+    const movieData = {
+      movieName: req.body.movieName,
+      imdbRating: req.body.imdbRating,
+      genre: req.body.genre,
+      releaseYear: req.body.releaseYear,
+      image: req.file ? req.file.filename : null,
+    };
+    let data = await MovieModel.create(movieData);
+    console.log(data);
+    res.status(200).send({ msg: "Data Added successfully", data: data });
+  } catch (error) {
+    res.status(401).send({ msg: error.message });
+  }
+});
 
-MovieRouter.post('/addmovie',Data.single("poster"),async(req,res)=>{
-    try {
-        const movieData = {
-            movieName: req.body.movieName,
-            imdbRating: req.body.imdbRating,
-            genre: req.body.genre,
-            releaseYear: req.body.releaseYear,
-            image: req.file ? req.file.filename : null  // Save the filename if file exists
-        };
-    let data=await MovieModel.create(movieData)
-        console.log(data)
-    res.status(200).send({msg:"Data Added successfully",data:data})
-    } catch (error) {
-        res.status(401).send({msg:error.message})
-    }    
-})
-
-module.exports=MovieRouter
+module.exports = MovieRouter;
