1ec64e0463d81c7d1b3bf7a0dcbc8b69503c4729
karan-raval <karanraval424@gmail.com>
2024-12-02 20:44:48 +0530

    Login Signup With Ejs

--- Day-16/Routes/MovieRouter.js
+++ Day-16/Routes/MovieRouter.js
@@ -2,4 +2,10 @@ const express =require('express')
 
 const Movierouter= express.Router()
 
+
+Movierouter.get('/',(req,res)=>{
+
+    res.render('LoginSignup.ejs')
+})
+
 module.exports=Movierouter
--- Day-16/app.js
+++ Day-16/app.js
@@ -1,10 +1,13 @@
 const express =require('express')
 const connect =require('./config/db')
+const Movierouter = require('./Routes/MovieRouter')
 const app=express()
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+
+app.use('/',Movierouter)
 
 app.listen(3122,()=>{
     connect()
