f5c3ed8cf0fa16273a9363884fc959fa657c97c6
karan-raval <karanraval424@gmail.com>
2024-12-02 13:50:09 +0530

    app.js in Ejs

--- Day-16/app.js
+++ Day-16/app.js
@@ -2,6 +2,10 @@ const express =require('express')
 const connect =require('./config/db')
 const app=express()
 
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('public'))
+
 app.listen(3122,()=>{
     connect()
     console.log('server running at 3122')
