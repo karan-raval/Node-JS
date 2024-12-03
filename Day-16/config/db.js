03773d5fbdcf94417be6d8cd4f1a1c9d38283a5e
karan-raval <karanraval424@gmail.com>
2024-12-03 11:32:35 +0530

    database connection

--- Day-16/config/db.js
+++ Day-16/config/db.js
@@ -1,7 +1,7 @@
  const mongoose =require('mongoose')
 
  const connect = async()=>{
-    await mongoose.connect('mongodb+srv://karan:raval@cluster0.howpl.mongodb.net/MovieData?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect('mongodb+srv://karan:raval@cluster0.howpl.mongodb.net/USERDATA?retryWrites=true&w=majority&appName=Cluster0')
     console.log('database conncetd susscessfully')
  }
 
