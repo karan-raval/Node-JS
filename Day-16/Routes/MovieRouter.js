a594753859ddcc8254eb84a0cf22d5364a08d1d5
karan-raval <karanraval424@gmail.com>
2024-12-03 10:16:59 +0530

    Login Signup With Ejs

--- Day-16/Routes/MovieRouter.js
+++ Day-16/Routes/MovieRouter.js
@@ -14,4 +14,33 @@ Movierouter.get('/signup',(req,res)=>{
 })
 
 
+UserRouter.post("/signup",async(req,res)=>{
+    const data = req.body;
+  try {
+       let a = new UserModel(data)
+       await a.save()
+       return res.redirect("/login")
+  } catch (error) {
+      res.status(401).json({msg : error.message})
+  }
+})
+
+
+UserRouter.post("/login",async(req,res)=>{
+    const {email,password}  = req.body
+  try {
+      let user = await UserModel.findOne({email : email})
+      if(!user){
+        return res.status(401).json({msg : "Email is Not Registered"})
+      }
+      if(user.password != password){
+          return res.status(401).json({msg : "Password Incorrect"})
+      }
+      res.status(200).json({msg : "User Login Successfully"})
+  } catch (error) {
+      res.status(401).json({msg : error.message})
+  }
+})
+
+
 module.exports=Movierouter
