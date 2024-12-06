7fb3eb78fc414d272e8c0114257a052631a25ad5
karan-raval <karanraval424@gmail.com>
2024-12-06 15:21:41 +0530

    app.js in Bolg App

--- Day-18(Project)/Backend/app.js
+++ Day-18(Project)/Backend/app.js
@@ -2,8 +2,11 @@ const express=require('express')
 
 const UserRouter = require('./Routes/UserRouter')
 const connect = require('./config/server')
+const cors=require('cors')
 const app=express()
 
+app.use(cors())
+app.use(express.json())
 app.use(express.static('public'))
 
 app.use('/',UserRouter)
