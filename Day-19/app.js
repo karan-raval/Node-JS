d1892fa3b941eba3049b88177130b8c1aca9c59a
karan-raval <karanraval424@gmail.com>
2024-12-09 13:05:17 +0530

    Passport js In Ejs

--- Day-19/app.js
+++ Day-19/app.js
@@ -1,10 +1,14 @@
 const express=require('express')
+const UserRouter = require('./Routes/UserRoutes')
 
 const app=express()
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+
+
+app.use('/user',UserRouter)
 
 
 
