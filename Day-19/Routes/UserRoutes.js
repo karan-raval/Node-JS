39e1650c455bb9076910e4e942f6362e542f4393
karan-raval <karanraval424@gmail.com>
2024-12-12 14:05:48 +0530

    Login Signup With Passport js

--- Day-19/Routes/UserRoutes.js
+++ Day-19/Routes/UserRoutes.js
@@ -1,6 +1,30 @@
 const {Router}=require('express')
+const userModel =require('../Model/UserSchema')
+const passport = require('passport')
+const LocalStratergy =require('passport-local').Strategy
 
 const UserRouter=Router()
+passport.use(new LocalStratergy(
+    async (username,password,done)=>{
+        let user = await userModel.findOne({username})
+        if(!user){
+            return done(null,false,{msg:"user not register"})
+        }
+        if(user.password != password){
+            return done(null,false,{msg:"incorrect password"})
+        }
+        return done(null,user)
+    }
+))
+
+passport.serializeUser((user,done)=>{
+    done(null,user.id)
+})
+
+passport.deserializeUser(async(id,done)=>{
+    let user=await userModel.findById(id)
+    done(null,user)
+})
 
 UserRouter.get('/',(req,res)=>{
 res.render('signup.ejs')
@@ -10,6 +34,5 @@ UserRouter.post('/',(req,res)=>{
     let data=req.body
 })
 
-UserRouter.post()
 
 module.exports=UserRouter
--- Day-19/app.js
+++ Day-19/app.js
@@ -1,12 +1,17 @@
 const express=require('express')
 const UserRouter = require('./Routes/UserRoutes')
 const connect =require('./Config/server')
+const passport = require('passport')
 
 const app=express()
 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
+
+app.use(session({secret : "karanraval2902"}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 app.use('/user',UserRouter)
