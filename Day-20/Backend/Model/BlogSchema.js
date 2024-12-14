74e8919986f005da711286eaa3234dbdee78b016
karan-raval <karanraval424@gmail.com>
2024-12-14 10:18:59 +0530

    Login signup With React

--- Day-20/Backend/Model/BlogSchema.js
+++ Day-20/Backend/Model/BlogSchema.js
@@ -6,6 +6,7 @@ heading: { type: String, require: true },
 des: { type: String, require: true },
 imgURL: { type: String, require: true },
 date: { type: String,},
+like:{type:Number,default:0}
 });
 const BLogModel = mongoose.model("BlogData", UserSchema);
 module.exports = BLogModel;
