a456074be1ebd15f80a5a8a77975f14a154004ef
karan-raval <karanraval424@gmail.com>
2024-12-16 12:44:13 +0530

    Login signup With bcrypt password

--- Day-21/Backend/Model/UserSchema.js
+++ Day-21/Backend/Model/UserSchema.js
@@ -1,13 +1,11 @@
-
 const mongooose = require("mongoose");
 
 const UserSchema = mongooose.Schema({
-    email : {type : String,require : true,unique : true},
-    password : {type : String, require : true},
-    username : {type : String, require : true}
-})
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  username: { type: String, require: true },
+});
 
- const UserModel = mongooose.model("users", UserSchema);
+const UserModel = mongooose.model("users", UserSchema);
 
-
- module.exports = UserModel
+module.exports = UserModel;
