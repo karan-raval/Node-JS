14ea712378f9f480531349c30b5716e3f9648927
karan-raval <karanraval424@gmail.com>
2024-12-19 12:47:52 +0530

    Login signup Forgot Password

--- Day-23/Backend/app.js
+++ Day-23/Backend/app.js
@@ -1,10 +1,14 @@
 const express =require('express')
 const cors=require('cors')
+const  connect  = require('./Config/server')
+require('dotenv').config()
 
 const app=express()
 app.use(cors())
 
 
-app.listen(9999,()=>{
-console.log('Server Running at 9999')
+
+app.listen(process.env.port,()=>{
+    connect()
+console.log(`Server Running at ${process.env.port}`)
 })
