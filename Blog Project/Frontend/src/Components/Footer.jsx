a77b008420ebd649fb3f81bc41524373a4e93181
karan-raval <karanraval424@gmail.com>
2024-12-30 13:12:18 +0530

    blog app project

--- Blog Project/Frontend/src/Components/Footer.jsx
+++ Blog Project/Frontend/src/Components/Footer.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const pxShow = 500;
+
+  const handleScroll = () => {
+    if (window.scrollY >= pxShow) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <footer className="s-footer">
@@ -8,88 +28,82 @@ const Footer = () => {
           <div className="row">
             <div className="col-two md-four mob-full s-footer__sitelinks">
               <h4>Quick Links</h4>
-
               <ul className="s-footer__linklist">
                 <li>
-                  <a  >Home</a>
+                  <Link to="/login">Login</Link>
                 </li>
                 <li>
-                  <a  >Blog</a>
+                  <Link to="/">Home</Link>
                 </li>
                 <li>
-                  <a  >Styles</a>
+                  <Link to="/createblog">Add Blog</Link>
                 </li>
                 <li>
-                  <a  >About</a>
+                  <Link to="/aboutus">About</Link>
                 </li>
                 <li>
-                  <a  >Contact</a>
+                  <a href="#contact">Contact</a>
                 </li>
                 <li>
-                  <a  >Privacy Policy</a>
+                  <a href="#privacy-policy">Privacy Policy</a>
                 </li>
               </ul>
             </div>
 
             <div className="col-two md-four mob-full s-footer__archives">
               <h4>Archives</h4>
-
               <ul className="s-footer__linklist">
                 <li>
-                  <a  >January 2018</a>
+                  <a href="#january-2018">January 2018</a>
                 </li>
                 <li>
-                  <a  >December 2017</a>
+                  <a href="#december-2017">December 2017</a>
                 </li>
                 <li>
-                  <a  >November 2017</a>
+                  <a href="#november-2017">November 2017</a>
                 </li>
                 <li>
-                  <a  >October 2017</a>
+                  <a href="#october-2017">October 2017</a>
                 </li>
                 <li>
-                  <a  >September 2017</a>
+                  <a href="#september-2017">September 2017</a>
                 </li>
                 <li>
-                  <a  >August 2017</a>
+                  <a href="#august-2017">August 2017</a>
                 </li>
               </ul>
             </div>
 
             <div className="col-two md-four mob-full s-footer__social">
               <h4>Social</h4>
-
               <ul className="s-footer__linklist">
                 <li>
-                  <a  >Facebook</a>
+                  <a >Facebook</a>
                 </li>
                 <li>
-                  <a  >Instagram</a>
+                  <a >Instagram</a>
                 </li>
                 <li>
-                  <a  >Twitter</a>
+                  <a >Twitter</a>
                 </li>
                 <li>
-                  <a  >Pinterest</a>
+                  <a >Pinterest</a>
                 </li>
                 <li>
-                  <a>Google+</a>
+                  <a >Google+</a>
                 </li>
                 <li>
-                  <a>LinkedIn</a>
+                  <a >LinkedIn</a>
                 </li>
               </ul>
             </div>
 
             <div className="col-five md-full end s-footer__subscribe">
               <h4>Our Newsletter</h4>
-
               <p>
-                Sit vel delectus amet officiis repudiandae est voluptatem.
-                Tempora maxime provident nisi et fuga et enim exercitationem
-                ipsam. Culpa consequatur occaecati.
+                Stay updated with our latest news and insights. Subscribe to our
+                newsletter.
               </p>
-
               <div className="subscribe-form">
                 <form id="mc-form" className="group">
                   <input
@@ -99,11 +113,29 @@ const Footer = () => {
                     placeholder="Email Address"
                     required
                   />
-
                   <input type="submit" name="subscribe" value="Send" />
-
-                  <label  className="subscribe-message"></label>
+                  <label className="subscribe-message"></label>
                 </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="s-footer__bottom">
+          <div className="row">
+            <div className="col-full">
+              <div className={`go-top ${isVisible ? "link-is-visible" : ""}`}>
+                <a
+                  className="smoothscroll"
+                  title="Back to Top"
+                  href="#top"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                  }}
+                >
+                  ↑
+                </a>
               </div>
             </div>
           </div>
