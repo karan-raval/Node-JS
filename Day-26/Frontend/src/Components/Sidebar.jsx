b1c7c78813f1011103d5315be95e848ba1860e98
karan-raval <karanraval424@gmail.com>
2025-01-10 16:47:00 +0530

    Admin Category added

--- Day-26/Frontend/src/Components/Sidebar.jsx
+++ Day-26/Frontend/src/Components/Sidebar.jsx
@@ -1,217 +1,203 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
   return (
     <>
-    <nav class="sidebar sidebar-offcanvas" id="sidebar">
-          <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-            <a class="sidebar-brand brand-logo">
-              <img src="https://demo.bootstrapdash.com/corona-free/jquery/template/assets/images/logo.svg" alt="logo" />
-            </a>
-            <a class="sidebar-brand brand-logo-mini" >
-              <img src="https://demo.bootstrapdash.com/corona-free/jquery/template/assets/images/logo-mini.svg" alt="logo" />
-            </a>
-          </div>
-          <ul class="nav">
-            <li class="nav-item profile">
-              <div class="profile-desc">
-                <div class="profile-pic">
-                  <div class="count-indicator">
-                    <img
-                      class="img-xs rounded-circle "
-                      src="assets/images/faces/face15.jpg"
-                      alt=""
-                    />
-                    <span class="count bg-success"></span>
-                  </div>
-                  <div class="profile-name">
-                    <h5 class="mb-0 font-weight-normal">Henry Klein</h5>
-                    <span>Gold Member</span>
-                  </div>
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+          <a class="sidebar-brand brand-logo">
+            <img
+              src="https://demo.bootstrapdash.com/corona-free/jquery/template/assets/images/logo.svg"
+              alt="logo"
+            />
+          </a>
+          <a class="sidebar-brand brand-logo-mini">
+            <img
+              src="https://demo.bootstrapdash.com/corona-free/jquery/template/assets/images/logo-mini.svg"
+              alt="logo"
+            />
+          </a>
+        </div>
+        <ul class="nav">
+          <li class="nav-item profile">
+            <div class="profile-desc">
+              <div class="profile-pic">
+                <div class="count-indicator">
+                  <img
+                    class="img-xs rounded-circle "
+                    src="assets/images/faces/face15.jpg"
+                    alt=""
+                  />
+                  <span class="count bg-success"></span>
                 </div>
-                <a  id="profile-dropdown" data-bs-toggle="dropdown">
-                  <i class="mdi mdi-dots-vertical"></i>
+                <div class="profile-name">
+                  <h5 class="mb-0 font-weight-normal">Henry Klein</h5>
+                  <span>Gold Member</span>
+                </div>
+              </div>
+              <a id="profile-dropdown" data-bs-toggle="dropdown">
+                <i class="mdi mdi-dots-vertical"></i>
+              </a>
+              <div
+                class="dropdown-menu dropdown-menu-right sidebar-dropdown preview-list"
+                aria-labelledby="profile-dropdown"
+              >
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <div class="preview-icon bg-dark rounded-circle">
+                      <i class="mdi mdi-settings text-primary"></i>
+                    </div>
+                  </div>
+                  <div class="preview-item-content">
+                    <p class="preview-subject ellipsis mb-1 text-small">
+                      Account settings
+                    </p>
+                  </div>
                 </a>
-                <div
-                  class="dropdown-menu dropdown-menu-right sidebar-dropdown preview-list"
-                  aria-labelledby="profile-dropdown"
-                >
-                  <a class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                      <div class="preview-icon bg-dark rounded-circle">
-                        <i class="mdi mdi-settings text-primary"></i>
-                      </div>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <div class="preview-icon bg-dark rounded-circle">
+                      <i class="mdi mdi-onepassword  text-info"></i>
                     </div>
-                    <div class="preview-item-content">
-                      <p class="preview-subject ellipsis mb-1 text-small">
-                        Account settings
-                      </p>
+                  </div>
+                  <div class="preview-item-content">
+                    <p class="preview-subject ellipsis mb-1 text-small">
+                      Change Password
+                    </p>
+                  </div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item preview-item">
+                  <div class="preview-thumbnail">
+                    <div class="preview-icon bg-dark rounded-circle">
+                      <i class="mdi mdi-calendar-today text-success"></i>
                     </div>
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a  class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                      <div class="preview-icon bg-dark rounded-circle">
-                        <i class="mdi mdi-onepassword  text-info"></i>
-                      </div>
-                    </div>
-                    <div class="preview-item-content">
-                      <p class="preview-subject ellipsis mb-1 text-small">
-                        Change Password
-                      </p>
-                    </div>
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a  class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                      <div class="preview-icon bg-dark rounded-circle">
-                        <i class="mdi mdi-calendar-today text-success"></i>
-                      </div>
-                    </div>
-                    <div class="preview-item-content">
-                      <p class="preview-subject ellipsis mb-1 text-small">
-                        To-do list
-                      </p>
-                    </div>
-                  </a>
-                </div>
+                  </div>
+                  <div class="preview-item-content">
+                    <p class="preview-subject ellipsis mb-1 text-small">
+                      To-do list
+                    </p>
+                  </div>
+                </a>
               </div>
-            </li>
-            <li class="nav-item nav-category">
-              <span class="nav-link">Navigation</span>
-            </li>
-            <li class="nav-item menu-items">
-              <span class="nav-link" >
-                <span class="menu-icon">
-                  <i class="mdi mdi-speedometer"></i>
-                </span>
-                <span class="menu-title">Dashboard</span>
+            </div>
+          </li>
+          <li class="nav-item nav-category">
+            <span class="nav-link">Navigation</span>
+          </li>
+          <li class="nav-item menu-items">
+            <span class="nav-link">
+              <span class="menu-icon">
+                <i class="mdi mdi-speedometer"></i>
               </span>
-            </li>
-            <li class="nav-item menu-items">
-              <a
-                class="nav-link"
-                data-bs-toggle="collapse"
-                aria-expanded="false"
-                aria-controls="ui-basic"
-              >
-                <span class="menu-icon">
-                  <i class="mdi mdi-laptop"></i>
-                </span>
-                <span class="menu-title">Basic UI Elements</span>
-                <i class="menu-arrow"></i>
-              </a>
-              <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" >
-                      Buttons
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" >
-                      Dropdowns
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a
-                      class="nav-link"
-                    >
-                      Typography
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item menu-items">
-              <a class="nav-link" >
-                <span class="menu-icon">
-                  <i class="mdi mdi-playlist-play"></i>
-                </span>
-                <span class="menu-title">Form Elements</span>
-              </a>
-            </li>
-            <li class="nav-item menu-items">
-              <a class="nav-link" >
-                <span class="menu-icon">
-                  <i class="mdi mdi-table-large"></i>
-                </span>
-                <span class="menu-title">Tables</span>
-              </a>
-            </li>
-            <li class="nav-item menu-items">
-              <a class="nav-link" >
-                <span class="menu-icon">
-                  <i class="mdi mdi-chart-bar"></i>
-                </span>
-                <span class="menu-title">Charts</span>
-              </a>
-            </li>
-            <li class="nav-item menu-items">
-              <a class="nav-link" >
-                <span class="menu-icon">
-                  <i class="mdi mdi-contacts"></i>
-                </span>
-                <span class="menu-title">Icons</span>
-              </a>
-            </li>
-            <li class="nav-item menu-items">
-              <a
-                class="nav-link"
-                data-bs-toggle="collapse"
-                aria-expanded="false"
-                aria-controls="auth"
-              >
-                <span class="menu-icon">
-                  <i class="mdi mdi-security"></i>
-                </span>
-                <span class="menu-title">User Pages</span>
-                <i class="menu-arrow"></i>
-              </a>
-              <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" >
-                      Blank Page
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" >
-                      404
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" >
-                      500
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" >
-                      Login
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" >
-                      Register
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item menu-items">
-              <a
-                class="nav-link"
-              >
-                <span class="menu-icon">
-                  <i class="mdi mdi-file-document-box"></i>
-                </span>
-                <span class="menu-title">Documentation</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+              <span class="menu-title">Dashboard</span>
+            </span>
+          </li>
+          <li class="nav-item menu-items">
+            <a
+              class="nav-link"
+              data-bs-toggle="collapse"
+              aria-expanded="false"
+              aria-controls="ui-basic"
+            >
+              <span class="menu-icon">
+                <i class="mdi mdi-laptop"></i>
+              </span>
+              <span class="menu-title">Add Category</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link">Buttons</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">Dropdowns</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">Typography</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link">
+              <span class="menu-icon">
+                <i class="mdi mdi-playlist-play"></i>
+              </span>
+              <span class="menu-title">Form Elements</span>
+            </a>
+          </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link">
+              <span class="menu-icon">
+                <i class="mdi mdi-table-large"></i>
+              </span>
+              <span class="menu-title">Tables</span>
+            </a>
+          </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link">
+              <span class="menu-icon">
+                <i class="mdi mdi-chart-bar"></i>
+              </span>
+              <span class="menu-title">Charts</span>
+            </a>
+          </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link">
+              <span class="menu-icon">
+                <i class="mdi mdi-contacts"></i>
+              </span>
+              <span class="menu-title">Icons</span>
+            </a>
+          </li>
+          <li class="nav-item menu-items">
+            <a
+              class="nav-link"
+              data-bs-toggle="collapse"
+              aria-expanded="false"
+              aria-controls="auth"
+            >
+              <span class="menu-icon">
+                <i class="mdi mdi-security"></i>
+              </span>
+              <span class="menu-title">User Pages</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                  <a class="nav-link">Blank Page</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">404</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">500</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link">Register</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link">
+              <span class="menu-icon">
+                <i class="mdi mdi-file-document-box"></i>
+              </span>
+              <span class="menu-title">Documentation</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
