5026a957e44f42c87d3df223bf37ed67027d9c38
karan-raval <karanraval424@gmail.com>
2025-01-10 11:38:18 +0530

    admin and user login signup

--- Day-25/Frontend/src/Pages/Login.jsx
+++ Day-25/Frontend/src/Pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import  jwtDecode  from 'jwt-decode';
+import  {jwtDecode}  from 'jwt-decode';
 
 const Login = () => {
   const [state, setState] = useState({
