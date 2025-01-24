b537d9be6d60f4b045476b7ac8d8773145cf302c
karan-raval <karanraval424@gmail.com>
2025-01-24 15:26:23 +0530

    traveling project

--- Traveling Project/Backend/app.js
+++ Traveling Project/Backend/app.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-const dotenv = require("dotenv")
+require("dotenv").config()
 
 const userRoutes = require("./routes/userRoutes")
 const bookingRoutes = require("./routes/bookingRoutes")
 
-dotenv.config()
 
 const app = express()
 
@@ -21,8 +20,8 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err))
 
-app.use("/api/users", userRoutes)
-app.use("/api/bookings", bookingRoutes)
+app.use("/", userRoutes)
+app.use("/", bookingRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
